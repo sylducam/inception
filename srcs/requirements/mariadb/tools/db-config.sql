8fcd8fe1f9973adf549f24d3d04d4d0fade4fdb3
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-05-12 11:58:56 -0400

    still broken with the sock error

--- srcs/requirements/mariadb/tools/db-config.sql
+++ srcs/requirements/mariadb/tools/db-config.sql
@@ -1,6 +1,8 @@
 /* rename the database */
 CREATE DATABASE IF NOT EXISTS wordpress;
 /* below is just a test, you will have to put the password in the .env file */
-GRANT ALL ON wordpress.* TO 'sylducam'@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;
+CREATE USER 'sylducam' IDENTIFIED BY 'blabla';
+GRANT ALL ON wordpress.* TO 'sylducam'@'%';
+UPDATE mysql.user SET plugin = '' WHERE user = 'sylducam' AND host = 'localhost';
 FLUSH PRIVILEGES;
-SHOW DATABASES;
+-- SHOW DATABASES;
--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -7,11 +7,17 @@
 
 #MYSQL
 # service mysql start
-cd /etc/init.d
-mysqld start
-cd /
+# add if condition above but get it right
+mysql_install_db --ldata=/var/lib/mysql --user=mysql
+mysqladmin -u root password "blabla"
+echo "test1\n"
+mv mariadb/tools/my.cnf /etc/mysql/my.cnf
+echo "test2\n"
 mysql -u root < mariadb/tools/db-config.sql
-/bin/sh /usr/bin/mysqld_safe
+chmod -R 755 /var/run/mysqld
+echo "test3\n"
+# /bin/sh /usr/bin/mysqld_safe
+mysqld_safe --bind-address=0.0.0.0
 # killall mysqld mysqld_safe
 # while :
 # do
