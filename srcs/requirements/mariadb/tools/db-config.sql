b2362ef894d8942f5c1ed2388cdbea363fa3b350
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-05-16 11:17:11 -0400

    env variables still don't work

--- srcs/requirements/mariadb/tools/db-config.sql
+++ srcs/requirements/mariadb/tools/db-config.sql
@@ -1,9 +1,8 @@
 -- rename the database
 CREATE DATABASE IF NOT EXISTS wordpress;
--- below is just a test, you will have to put the password in the .env file
 CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%';
 -- what's the use of the next line ?
-UPDATE mysql.user SET plugin = '' WHERE user = 'sylducam' AND host = 'localhost';
+-- UPDATE mysql.user SET plugin = '' WHERE user = 'sylducam' AND host = 'localhost';
 FLUSH PRIVILEGES;
 -- SHOW DATABASES;
--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -12,6 +12,13 @@
 # add if condition above but get it right
 exec /usr/bin/mysqld_safe
 
+# mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;
+# # CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD' \
+# # GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%' \
+# # FLUSH PRIVILES;"
+
+# mysql -u root < db-config.sql
+
 # mysqld_safe --bind-address=0.0.0.0
 
 # mysql_install_db --ldata=/var/lib/mysql --user=mysql
