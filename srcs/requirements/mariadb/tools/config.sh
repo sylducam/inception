4468401e9ec627b5bc5c4a1ef2515474e67dda4d
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-05-17 09:15:17 -0400

    .sql seems to be working. do the tests again

--- srcs/requirements/mariadb/tools/config.sh
+++ srcs/requirements/mariadb/tools/config.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+sed -i "s/DB_NAME/$DB_NAME/g"							"$DB_TOOLS/db-config.sql"
+sed -i "s/MYSQL_USER/$MYSQL_USER/g"						"$DB_TOOLS/db-config.sql"
+sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g"				"$DB_TOOLS/db-config.sql"
+sed -i "s/MYSQL_ROOT_PASSWORD/$MYSQL_ROOT_PASSWORD/g"	"$DB_TOOLS/db-config.sql"
+cat $DB_TOOLS/db-config.sql 
+
 service mysql start
-mv mariadb/tools/my.cnf /etc/mysql/my.cnf
-mysql -u root < mariadb/tools/db-config.sql
+mv $DB_TOOLS/my.cnf /etc/mysql/my.cnf
+mysql -u root < $DB_TOOLS/db-config.sql
+echo coucou
 sleep 1 # Waiting for mysql to finish the edit
-service mysql stop
+# service mysql stop
--- srcs/requirements/mariadb/tools/db-config.sql
+++ srcs/requirements/mariadb/tools/db-config.sql
@@ -1,8 +1,8 @@
 -- rename the database
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER '$MYSQL_USER' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL ON wordpress.* TO '$MYSQL_USER'@'%';
+CREATE DATABASE IF NOT EXISTS DB_NAME;
+CREATE USER 'MYSQL_USER' IDENTIFIED BY 'MYSQL_PASSWORD';
+GRANT ALL ON DB_NAME.* TO 'MYSQL_USER'@'%';
 -- what's the use of the next line ?
 -- UPDATE mysql.user SET plugin = '' WHERE user = 'sylducam' AND host = 'localhost';
 FLUSH PRIVILEGES;
--- SHOW DATABASES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'MYSQL_ROOT_PASSWORD';
