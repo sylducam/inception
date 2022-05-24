8403aea98c27dd75d7d5ee5af905105062774852
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-05-24 08:08:56 -0400

    same socket issue for wp that I had for mariadb, only this time the .sock and php folders don't even exist. Don't forget to comment nginx part in your yml

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
 
-# under which condition sould I execute or not execute this script ?
-# if []
-	# then
-# fi
-# while :
-# do
-# 	echo "Do something; hit [CTRL+C] to stop!"
-# done
-#MYSQL
-# add if condition above but get it right
-exec /usr/bin/mysqld_safe
-
-# mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;
-# # CREATE USER_NAME '$USER_NAME' IDENTIFIED BY '$USER_PASSWORD' \
-# # GRANT ALL ON wordpress.* TO '$USER_NAME'@'%' \
-# # FLUSH PRIVILES;"
-
-# mysql -u root < db-config.sql
-
-# mysqld_safe --bind-address=0.0.0.0
-
-# mysql_install_db --ldata=/var/lib/mysql --user=mysql
-# mysqladmin -u root password "blabla"
+# need condition
+exec /usr/sbin/php-fpm7.3 -F -R
