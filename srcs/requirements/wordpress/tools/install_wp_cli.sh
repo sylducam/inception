686edc774f959346de4077baae3f15411a6e062f
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-06-01 07:11:07 -0400

    wp core command still doesn't work

--- srcs/requirements/wordpress/tools/install_wp_cli.sh
+++ srcs/requirements/wordpress/tools/install_wp_cli.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # wp-cli installation
-exec curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-exec chmod +x wp-cli.phar
-exec mv wp-cli.phar /usr/local/bin/wp
-exec wp core install --url=sylducam.42.fr --title=${DB_NAME} --admin_user=${USER_NAME} --admin_password=${USER_PASSWORD} --allow-root --path=/var/www/html/wordpress
+# echo ICI0
+# exec curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# echo ICI1
+# exec chmod +x wp-cli.phar
+# echo ICI2
+# exec mv wp-cli.phar /usr/local/bin/wp
+echo ICI3
+exec wp core install --url=sylducam.42.fr --title="$DB_NAME" --admin_user="$USER_NAME" \
+--admin_password="$USER_PASSWORD" --admin_email=sylducam@gmail.com --allow-root \
+--path=/var/www/html/wordpress
+echo ICI4
 
