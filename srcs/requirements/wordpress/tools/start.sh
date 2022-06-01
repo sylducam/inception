865499c7fdec94d7bef3e34bf40bd88feeb964cb
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-06-01 11:00:26 -0400

    project seems finished. just need to verufy everything and clean

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -6,5 +6,8 @@ set -e # exit if commands below have exit code != 0
 wp core install --url=sylducam.42.fr --title="$DB_NAME" --admin_user="$USER_NAME" \
 --admin_password="$USER_PASSWORD" --admin_email=sylducam@gmail.com --allow-root \
 --path=/var/www/html/wordpress
+sleep 10
+# echo ICI
+wp user create --path=/var/www/html/wordpress "$WP_USER_NAME" "$WP_USER_EMAIL" --allow-root --user_pass="$WP_USER_PASSWORD" || true
 
 exec /usr/sbin/php-fpm7.3 -F -R
