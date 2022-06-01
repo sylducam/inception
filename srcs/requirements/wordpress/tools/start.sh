440e40eb6a490b529944a558f967de223c325fd8
Sylvain DUCAMP <sylducam@debian-BULLSEYE-live-builder-AMD64>
2022-06-01 04:02:28 -0400

    trying to copy .vimrc from volumes to right place in containers but I have an issue with my volumes

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # need condition
+# exec wp core install --url=sylducam.42.fr --title=${DB_NAME} --admin_user=${USER_NAME} --admin_password=${USER_PASSWORD} --allow-root --path=/var/www/html/wordpress
 exec /usr/sbin/php-fpm7.3 -F -R
